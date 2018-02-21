cab23beefd4fdab3665faf3128c251eaf643fb1b
Milka404 <Milka2158@gmail.com>
2018-02-21 15:05:49 +0700

    1proga

---
+++ nini.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+main ()
+{
+	int a, b=5;
+	for(int i=0; i<b; i++)
+	{
+		 a+=i*b;
+		 cout<<a;
+
+	}
+	
+	cout << "Summa "<< a<< endl;
+}
